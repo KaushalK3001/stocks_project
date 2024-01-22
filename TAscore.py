3fe8dfa56d2db342bde173f57f0b65e0d7d2d25d
kaushal3001 <kaushal.kirpekar@gmail.com>
2024-01-22 10:15:07 +0530

    TA score endpoint

---
+++ TAscore.py
@@ -0,0 +1,6 @@
+
+
+def calculate_TAscore(stock):
+    # implement web sccraping to return score out of 10
+
+
--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+from TAscore import calculate_TAscore
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/technical_analysis_score/<stock>')
+def technical_analysis_score(stock):
+    # Call the function to calculate technical analysis score
+    ta_score = calculate_TAscore(stock)
+    
+    # Pass the score to the template
+    return render_template('technical_analysis_score.html', stock_name=stock, ta_score=ta_score)
 
 if __name__ == '__main__':
     app.run(debug=True)
